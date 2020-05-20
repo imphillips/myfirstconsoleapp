72848012e28aaa6ae2b2bc08dd2f91ab3772b559
isisphillips <isis_phillips@yahoo.com>
2020-05-19 20:00:15 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myfirstconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Isis!");
+        }
+    }
+}
